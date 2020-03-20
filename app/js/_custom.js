8c81544571ad9ed17f34e7eee72f0f7b5f237349
il97mur <ilha9749@gmail.com>
2020-03-20 13:03:52 +0600

    second commit
    Half of mainpage is ready (not adaptive, only for >1440px)

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -2,4 +2,31 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	// Custom JS
 
+	$(document).on('click', function(e) {
+		console.log(e.target)
+	});
+
+	$('.j-slide').on('click', function(){
+
+		$(this).next().slideToggle();
+	
+		$(this).next().css('display', 'flex');
+		$(this).find('img').toggleClass('rotate');
+	
+		if ($(this).css('margin-bottom') == '0px') {
+			$(this).css('margin-bottom', '75px');
+		} else {
+			$(this).css('margin-bottom', '0px');
+		}
+	});
+
+	$('.close').on('click', function(){
+		$('.popup-menu').fadeToggle();
+		console.log('Успешный клик');
+	});
+	
+	$('.hamburger').on('click', function(){
+		$('.popup-menu').fadeToggle();
+	});
+
 });
